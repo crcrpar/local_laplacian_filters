d069c88af890ffce14eefaa0766df079d5aef9a6
crcrpar <masakikozuki@Masaki-no-MacBook-Pro.local>
2017-01-08 23:44:11 +0900

    change style

--- opencv_utils.h
+++ opencv_utils.h
@@ -7,37 +7,23 @@
 #include <opencv/cv.h>
 #include <vector>
 
-cv::Mat ByteScale(const cv::Mat& input,
-                  bool verbose = false);
+cv::Mat ByteScale(const cv::Mat& input, bool verbose = false);
 
-void ByteScale(const cv::Mat& input,
-               cv::Mat& output,
-               bool verbose = false);
+void ByteScale(const cv::Mat& input, cv::Mat& output, bool verbose=false);
 
-cv::Mat ByteScale(const cv::Mat& input,
-                  double* min,
-                  double* max,
-                  bool verbose = false);
+cv::Mat ByteScale(const cv::Mat& input, double* min,
+  double* max, bool verbose = false);
 
-void ByteScale(const cv::Mat& input,
-               cv::Mat& output,
-               double* min,
-               double* max,
-               bool verbose = false);
+void ByteScale(const cv::Mat& input, cv::Mat& output, double* min,
+  double* max, bool verbose = false);
 
-cv::Mat ByteScale(const cv::Mat& input,
-                  double min,
-                  double max,
-                  bool verbose = false);
+cv::Mat ByteScale(const cv::Mat& input, double min,
+  double max, bool verbose = false);
 
-void ByteScale(const cv::Mat& input,
-               cv::Mat& output,
-               double min,
-               double max,
-               bool verbose = false);
+void ByteScale(const cv::Mat& input, cv::Mat& output, double min,
+  double max, bool verbose = false);
 
-void LogScale(const cv::Mat& input,
-              cv::Mat& output);
+void LogScale(const cv::Mat& input, cv::Mat& output);
 
 cv::Mat LogScale(const cv::Mat& input);
 
@@ -47,16 +33,11 @@ cv::Mat magnitude(const cv::Mat& input);
 void magnitude(const cv::Mat& input, cv::Mat& output);
 
 
-cv::Mat circshift(const cv::Mat& src,
-                  cv::Point2f delta,
-                  int fill=cv::BORDER_CONSTANT,
-                  cv::Scalar value=cv::Scalar(0,0,0,0));
+cv::Mat circshift(const cv::Mat& src, cv::Point2f delta,
+  int fill=cv::BORDER_CONSTANT, cv::Scalar value=cv::Scalar(0,0,0,0));
 
-void circshift(const cv::Mat& src,
-               cv::Mat& dst,
-               cv::Point2f delta,
-               int fill=cv::BORDER_CONSTANT,
-               cv::Scalar value=cv::Scalar(0,0,0,0));
+void circshift(const cv::Mat& src, cv::Mat& dst, cv::Point2f delta,
+  int fill=cv::BORDER_CONSTANT, cv::Scalar value=cv::Scalar(0,0,0,0));
 
 void FFTShift(const cv::Mat& input, cv::Mat& output);
 cv::Mat FFTShift(const cv::Mat& input);
